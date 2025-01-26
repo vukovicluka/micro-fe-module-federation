ad4a61d1438e6a63c9f5356e9e3b1c5640cbfd37
Luka Vukovic <vukovicluka227@gmail.com>
2025-01-26 16:59:30 +0100

    Add CSS modules

--- apps/host/module-federation.config.ts
+++ apps/host/module-federation.config.ts
@@ -1,7 +1,17 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
 
+const sharedLibraries = [
+  "react",
+  "react-dom"
+]
+
 const config: ModuleFederationConfig = {
   name: 'host',
-  remotes: [['products', 'products@http://localhost:4201/remoteEntry.js']],
+  remotes: [['products', 'products@http://localhost:4201/mf-manifest.json']],
+  shared: (libraryName, _sharedConfig) => {
+    if (!sharedLibraries.includes(libraryName)) {
+      return false
+    }
+  }
 };
 export default config;
--- apps/products/src/components/remote-button.tsx
+++ apps/products/src/components/remote-button.tsx
@@ -1,3 +1,15 @@
+import { useState } from 'react';
+import styles from './remote-button.module.css';
+
 export default function Button() {
-  return <button type="button">Remote Button</button>
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button type="button" onClick={() => setCount((prev) => prev + 1)} className={styles.button}>
+        Remote button
+      </button>
+    </div>
+  );
 }
