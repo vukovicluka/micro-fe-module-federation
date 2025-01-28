e482bb053629ba00776f762cef3991372ea0693b
Luka Vukovic <vukovicluka227@gmail.com>
2025-01-28 14:16:01 +0100

    Log shared scope for observability

--- apps/host/dynamic-remote.ts
+++ apps/host/dynamic-remote.ts
@@ -1,9 +1,31 @@
 import { FederationRuntimePlugin } from '@module-federation/enhanced/runtime';
+import { Shared, ShareScopeMap } from '@module-federation/runtime/types';
 
 // async function fetchRemote() {
 //   // fetch remote URL from config service
 //   fetch("...")
 // }
+
+interface SimplifiedShare {
+  [pkgName: string]: {
+    version: string;
+    usedIn: string[];
+    from: string;
+  };
+}
+
+function simplifyShareScope(shareScope: ShareScopeMap['']): SimplifiedShare {
+  return Object.entries(shareScope).reduce((acc, [packageName, versions]) => {
+    const [version, details] = Object.entries(versions)[0];
+    const shared = details as unknown as Shared;
+    acc[packageName] = {
+      version,
+      usedIn: shared.useIn,
+      from: shared.from,
+    };
+    return acc;
+  }, {} as SimplifiedShare);
+}
 
 const runtimePlugin: () => FederationRuntimePlugin = function () {
   return {
@@ -22,6 +44,14 @@ const runtimePlugin: () => FederationRuntimePlugin = function () {
       remote.entry = 'http://localhost:4201/mf-manifest.json';
 
       return params;
+    },
+    async onLoad(args) {
+      const { options } = args;
+      // default -> Federation creates by default unless customized
+      const sharedScope = options.host.shareScopeMap['default'];
+      const simplified = simplifyShareScope(sharedScope);
+      // window.__FEDERATION__
+      console.log(simplified);
     },
   };
 };
